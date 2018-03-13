74ab5f72dacd91e42df4e3b24b5a09b4a56cdbfc
Alfredo Rodriguez <enriquea.rodriguezr@gmail.com>
2018-03-13 15:51:07 -0500

    first commit

---
+++ app/routes.py
@@ -0,0 +1,14 @@
+from flask import render_template
+from app import app
+from bs4 import BeautifulSoup
+import requests
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+
+	return render_template('index.html')
+
+@app.route('/scrap')
+def scrap()
